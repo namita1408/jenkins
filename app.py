0d84a96dac37a2b613eb0c236b8a18c086559e2e
namita1408 <sathishnamita@gmail.com>
2026-01-21 17:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 def add(a, b):
-return a + b
-if __name__ == &quot;__main__&quot;:
-print(&quot;Sum is:&quot;, add(2, 3))
+    return a + b
+
+
+if __name__ == "__main__":
+    result = add(2, 3)
+    print(f"Sum is: {result}")
